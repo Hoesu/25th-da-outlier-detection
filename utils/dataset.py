a0efdf9944703ae568f99a30c11421538a7d8e29
Hoesu Chung <treecollector@yonsei.ac.kr>
2024-10-06 02:11:08 +0900

    UPDATE

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,12 +1,14 @@
 import os
 import json
+import torch
 import pandas as pd
+import numpy as np
 from torch.utils.data import Dataset
 
 class OutlierDataset(Dataset):
     def __init__(self, config):
         self.config = config
-        self.data = None
+        self.data = self.prepare_data()
 
     def __len__(self):
         return len(self.data)
@@ -30,7 +32,8 @@ class OutlierDataset(Dataset):
         interval_path = self.config['interval_path']
         dirc_name = data_path.split('/')[1]
         file_name = data_path.split('/')[2]
-        interval = json.load(interval_path)[dirc_name][file_name]
+        with open(interval_path, 'r') as file:
+            interval = json.load(file)[dirc_name][file_name]
         return interval
     
     def split_by_interval(self,
@@ -45,29 +48,61 @@ class OutlierDataset(Dataset):
             subsets.append(subset)
         return subsets
 
-    def slice_by_window(self, data: list[pd.DataFrame]) -> list[list[float]]:
+    def slice_by_window(self, data: list[pd.DataFrame]) -> list[np.ndarray]:
         """
         분할된 데이터프레임별로 주어진 윈도우로 스텝 사이즈 만큼 이동하며 데이터 추출.
         만약에 주어진 구간 안에서 윈도우 설정이 불가능하면 해당 구간을 건너뛴다.
         """
         window_size = self.config['seq_size']
         step_size = self.config['step_size']
-        pass
+        windows = []
+        for subset in data:
+            values = subset['value'].to_numpy()
+            for start in range(0, len(values) - window_size + 1, step_size):
+                window = values[start:start + window_size]
+                windows.append(window)
+        return windows
 
-    def standardize(self, data: list[list[float]]) -> list[list[float]]:
+    def standardize(self, data: list[np.ndarray]) -> torch.Tensor:
         """
-        입력 배열을 순서를 유지한채로 1차원으로 변환하고, 정규화를 진행.
-        정규화한 값들을 다시 입력값과 같은 차원으로 변환.
+        모든 값들을 정규화, [seq_size, batch_size, 1] 차원의 텐서 반환.
         """
-        pass
+        data_array = np.array(data)
+        means = data_array.mean()
+        stds = data_array.std()
+        normalized_data = (data_array - means) / stds
+        return torch.tensor(normalized_data.T, dtype=torch.float32).unsqueeze(-1)
 
-    def add_noise(self, data: list[list[float]]) -> list[list[float]]:
+    def add_noise(self, data: torch.Tensor) -> torch.Tensor:
         """
         모든 값에 평균을 0, 표준편차를 1로 하는 정규분포로부터 샘플링된 노이즈를 추가한다.
         """
-        pass
+        noise = torch.normal(0, 0.01, data.size())
+        return data + noise
 
-    def prepare_data(self, config):
+    def process_test(self, data: pd.DataFrame) -> list[np.ndarray]:
         """
+        테스트용 데이터셋 생성시 원본 데이터프레임을 [-1, seq_size] 형태의 2차원 배열로 반환.
         """
-        pass
+        seq_size = self.config['seq_size']
+        values = data['value'].to_numpy()
+        return [values[i:i + seq_size] for i in range(len(values) - seq_size + 1)]
+
+    def prepare_data(self) -> torch.Tensor:
+        """
+        훈련, 테스트 케이스에 따라서 데이터 전처리 진행.
+        """
+        train = self.config['train']
+        batch_size = self.config['batch_size']
+        data = self.load_csv()
+
+        if train:
+            intervals = self.load_json()
+            data = self.split_by_interval(data, intervals)
+            data = self.slice_by_window(data)
+            data = self.standardize(data)
+            data = self.add_noise(data)
+        else:
+            data = self.process_test(data)
+            data = self.standardize(data)
+        return data
